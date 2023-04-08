06e8fa83d2c9cd343b8416f61dfa543437301d1a
Vignan Thumma <126392049+vignan8@users.noreply.github.com>
2023-04-08 08:29:22 +0530

    Sick CSS redesign yo

--- src/index.js
+++ src/index.js
@@ -6,252 +6,272 @@ import './index.css';
 const LOBBY_NAME = "twitchtactoe-lobby";
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
-  }
-  let filledSquares = 0;
-  for (let i = 0; i < squares.length; i++) {
-    if(squares[i]) {
-      filledSquares++;
+    let filledSquares = 0;
+    for (let i = 0; i < squares.length; i++) {
+        if (squares[i]) {
+            filledSquares++;
+        }
     }
-  }
-  if (filledSquares === squares.length) {
-    return 'draw';
-  } else {
-    return null;
-  }
+    if (filledSquares === squares.length) {
+        return 'draw';
+    } else {
+        return null;
+    }
 }
 
 function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick} >
-      {props.value}
-    </button>
-  );
+    return ( <
+        button className = "square"
+        onClick = {
+            props.onClick
+        } > {
+            props.value
+        } <
+        /button>
+    );
 }
 
 function LobbyList(props) {
-  const friends = props.friends;
-	const listItems = friends.map((number) =>
-    <li onClick={() => {document.getElementById('remotepeer').value=number;}} key={number}>{number}</li>
-  );
-  return (
-    <ul>{listItems}</ul>
-  );
+    const friends = props.friends;
+    const listItems = friends.map((number) =>
+        <
+        li onClick = {
+            () => {
+                document.getElementById('remotepeer').value = number;
+            }
+        }
+        key = {
+            number
+        } > {
+            number
+        } < /li>
+    );
+    return ( <
+        ul > {
+            listItems
+        } < /ul>
+    );
 }
 
 const states = {
-  NOT_CONNECTED: "not_connected",
-  PLAYER_X: "player_x",
-  PLAYER_O: "player_o"
+    NOT_CONNECTED: "not_connected",
+    PLAYER_X: "player_x",
+    PLAYER_O: "player_o"
 };
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-      peer: new Peer(),
-      peer_id: null,
-      conn: null,
-      connState: states.NOT_CONNECTED,
-      inlobby: [],
-    };
-    this.state.peer.on('open', (id) => {
-      this.setState({peer_id: id});
-      var lconn = this.state.peer.connect(LOBBY_NAME);
-      lconn.on('open', () => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+            peer: new Peer(),
+            peer_id: null,
+            conn: null,
+            connState: states.NOT_CONNECTED,
+            inlobby: [],
+        };
+        this.handleLobbyConnection = this.handleLobbyConnection.bind(this);
+        this.handlePlayerConnection = this.handlePlayerConnection.bind(this);
+        this.handlePlayerData = this.handlePlayerData.bind(this);
+        this.handleFakeClick = this.handleFakeClick.bind(this);
+
+        this.state.peer.on('open', (id) => {
+            this.setState({
+                peer_id: id
+            });
+            var lconn = this.state.peer.connect(LOBBY_NAME);
+            lconn.on('open', this.handleLobbyConnection);
+            lconn.on('data', (data) => {
+                console.log("setting lobby", data);
+                this.setState({
+                    inlobby: data
+                });
+            });
+        });
+
+        this.state.peer.on('connection', (conn) => {
+            console.log("got connection from", conn.peer);
+            if (this.state.conn == null) {
+                this.setState({
+                    conn: conn,
+                    connState: states.PLAYER_O
+                });
+                conn.on('data', this.handlePlayerData);
+            } else {
+                console.log("already connected");
+                conn.close();
+            }
+        });
+    }
+
+    handleLobbyConnection() {
         console.log("connected to lobby");
         var lobby_query = () => {
-          lconn.send("QUERY");
-          if (this.state.connState === states.NOT_CONNECTED) {
-            lconn.send("READY");
-          }
-          window.setTimeout(lobby_query, 1000);
+            this.state.peer.connect(LOBBY_NAME).send("QUERY");
+            if (this.state.connState === states.NOT_CONNECTED) {
+                this.state.peer.connect(LOBBY_NAME).send("READY");
+            }
+            window.setTimeout(lobby_query,
+                5000
+            );
         }
-        lobby_query();
-      });
-      lconn.on('data', (data) => {
-        console.log("setting lobby", data);
-        this.setState({inlobby: data});
-      });
-    });
-    this.state.peer.on('connection', (conn) => {
-      console.log("got connection from", conn.peer);
-      if (this.state.conn == null) {
-        this.setState({conn: conn, connState: states.PLAYER_O});
-        conn.on('data', (data) => {
-          console.log('Received', data);
-          if (this.state.xIsNext) {
-            // handle X press
-            this.handleFakeClick(Number(data));
-          }
-        });
-      } else {
-        console.log("already connected");
-        conn.close();
-      }
-    });
-  }
-
-  handleClick(i) {
-    if (this.state.connState === states.PLAYER_X && this.state.xIsNext) {
-      this.handleFakeClick(i);
-    } else if (this.state.connState === states.PLAYER_O && !this.state.xIsNext) {
-      this.handleFakeClick(i);
     }
-  }
-
-  handleFakeClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    this.state.conn.send(i);
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  renderSquare(i) {
-    return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
-    />;
-  }
-
-  connect() {
-    var rp = document.getElementById("remotepeer").value;
-    console.log("connect to", rp);
-    var conn = this.state.peer.connect(rp);
-    conn.on('open', () => {
-      console.log("connection open");
-      this.setState({conn: conn, connState: states.PLAYER_X});
-    });
-    conn.on('data', (data) => {
-      console.log('Received back', data);
-      if (!this.state.xIsNext) {
-        // handle O press
-        this.handleFakeClick(Number(data));
-      }
-    });
-  }
-
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let connstatus = this.state.connState;
-    let status;
-    if (winner != null) {
-      if (winner === 'draw') {
-        status = 'Game is a draw';
-      } else {
-        status = 'Winner: ' + winner;
-      }
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    handlePlayerConnection() {
+        const remotePeerId = document.getElementById('remotepeer').value;
+        if (remotePeerId !== "") {
+            var conn = this.state.peer.connect(remotePeerId);
+            conn.on('open', () => {
+                console.log("connected to player", remotePeerId);
+                this.setState({
+                    conn: conn,
+                    connState: states.PLAYER_X
+                });
+                conn.on('data', this.handlePlayerData);
+            });
+        }
     }
 
-    return (
-      <div>
-        <div className="connstatus">{connstatus}</div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-        <div class="peer-id">My peer id is: <span class="id-code">{this.state.peer_id}</span></div>
-        <input type="text" placeholder="remote peer id" id="remotepeer" />
-        <input class="connect-button" type="submit" value="connect" onClick={() => this.connect()} />
-        <div class="lobby">
-				<h3>Click a user to challenge</h3>
-        <div class="list"><LobbyList friends={this.state.inlobby} /></div>
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  componentDidMount() {
-    console.log("trying to create lobby");
-
-    let peers = {};
-
-    // this may fail unless you are the first player
-    var lobby = new Peer(LOBBY_NAME);
-    lobby.on('open', function(id) {
-      console.log('Lobby peer ID is: ' + id);
-    });
-
-    lobby.on('connection', (conn) => {
-      console.log('lobby connection', conn.peer);
-      conn.on('data', (data) => {
-        if (data === "READY") { 
-          peers[conn.peer] = (new Date()).getTime();
+    handlePlayerData(data) {
+        console.log("received data", data);
+        if (data.type === "move") {
+            const squares = this.state.squares.slice();
+            squares[data.move] = this.state.xIsNext ? 'X' : 'O';
+            const winner = calculateWinner(squares);
+            this.setState({
+                squares: squares,
+                xIsNext: !this.state.xIsNext,
+                winner: winner
+            });
+            if (winner) {
+                this.state.conn.send({
+                    type: "gameover",
+                    winner: winner
+                });
+            }
+        } else if (data.type === "gameover") {
+            this.setState({
+                winner: data.winner
+            });
         }
-        if (data === "QUERY") { 
-          conn.send(Object.keys(peers));
-        }
-      });
-    });
-
-    function expire() {
-      for (var k in peers) {
-        var now = (new Date()).getTime();
-        if (now - peers[k] > 3000) {
-          delete peers[k];
-        }
-      }
-      window.setTimeout(expire, 1000);
     }
-    expire();
-  }
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+
+    handleFakeClick(i) {
+        if (this.state.conn && this.state.xIsNext && !this.state.winner) {
+            const squares = this.state.squares.slice();
+            if (squares[i] || this.state.xIsNext !== true) {
+                return;
+            }
+            squares[i] = 'X';
+            const winner = calculateWinner(squares);
+            this.setState({
+                squares: squares,
+                xIsNext: false,
+                winner: winner
+            });
+            this.state.conn.send({
+                type: "move",
+                move: i
+            });
+            if (winner) {
+                this.state.conn.send({
+                    type: "gameover",
+                    winner: winner
+                });
+            }
+        }
+    }
+
+    renderSquare(i) {
+        return ( <
+            Square value = {
+                this.state.squares[i]
+            }
+            onClick = {
+                () => this.handleFakeClick(i)
+            }
+            />
+        );
+    }
+
+    render() {
+        let status;
+        if (this.state.winner) {
+            status = 'Winner: ' + this.state.winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        return ( <
+            div >
+            <
+            div className = "game-status" > {
+                status
+            } < /div> <
+            div className = "game-board" >
+            <
+            div className = "board-row" > {
+                this.renderSquare(0)
+            } {
+                this.renderSquare(1)
+            } {
+                this.renderSquare(2)
+            } <
+            /div> <
+            div className = "board-row" > {
+                this.renderSquare(3)
+            } {
+                this.renderSquare(4)
+            } {
+                this.renderSquare(5)
+            } <
+            /div> <
+            div className = "board-row" > {
+                this.renderSquare(6)
+            } {
+                this.renderSquare(7)
+            } {
+                this.renderSquare(8)
+            } <
+            /div> < /
+            div > <
+            div >
+            <
+            input type = "text"
+            id = "remotepeer"
+            placeholder = "remote peer id" / >
+            <
+            button onClick = {
+                this.handlePlayerConnection
+            } > Connect < /button> < /
+            div > <
+            LobbyList friends = {
+                this.state.inlobby
+            }
+            /> < /
+            div >
+        );
+    }
 }
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+ReactDOM.render( <
+    Board / > ,
+    document.getElementById('root')
 );
-
